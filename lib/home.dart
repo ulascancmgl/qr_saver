261ea256823d0bfbdb9dbc76ff89c91b77e8b2da
ulascancmgl <ulascancmgl@gmail.com>
2023-05-28 01:20:24 +0300

    adding google add

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'favorite.dart';
 import 'qr_code.dart';
@@ -19,10 +20,40 @@ class _HomePageState extends State<HomePage> {
     'Favoriler': 'Favorites',
   };
 
+  late BannerAd _bannerAd;
+  bool _isBannerAdReady = false;
+
   @override
   void initState() {
     super.initState();
     _loadSelectedLanguage();
+    _initializeBannerAd();
+  }
+
+  void _initializeBannerAd() {
+    _bannerAd = BannerAd(
+      adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+      size: AdSize.largeBanner,
+      request: AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBannerAdReady = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+        },
+      ),
+    );
+
+    _bannerAd.load();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd.dispose();
+    super.dispose();
   }
 
   Future<void> _loadSelectedLanguage() async {
@@ -96,108 +127,126 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       backgroundColor: Colors.transparent,
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.lightBlue.withOpacity(0.6),
-              Colors.deepPurpleAccent.withOpacity(0.6)
-            ],
-            begin: Alignment.bottomLeft,
-            end: Alignment.topRight,
-          ),
-          image: DecorationImage(
-            image: AssetImage('assets/images/pattern.png'), // Desenin yolu
-            repeat: ImageRepeat.repeat, // Desenin tekrarlanma şekli
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 200,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            MyQRCodePage(currentLanguage: currentLanguage),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurpleAccent,
-                    foregroundColor: Colors.white,
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    textStyle: TextStyle(fontSize: 18, fontFamily: 'Helvetica'),
-                    elevation: 4,
-                    shadowColor: Colors.blueAccent.withOpacity(0.3),
-                  ),
-                  child: Text(_getTranslatedString('QR kodu tara')),
-                ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.lightBlue.withOpacity(0.6),
+                  Colors.deepPurpleAccent.withOpacity(0.6)
+                ],
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
               ),
-              SizedBox(height: 16),
-              SizedBox(
-                width: 200,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            URLListPage(currentLanguage: currentLanguage),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurpleAccent,
-                    foregroundColor: Colors.white,
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    textStyle: TextStyle(fontSize: 18, fontFamily: 'Helvetica'),
-                    elevation: 4,
-                    shadowColor: Colors.blueAccent.withOpacity(0.3),
-                  ),
-                  child: Text(_getTranslatedString('Geçmiş')),
-                ),
+              image: DecorationImage(
+                image: AssetImage('assets/images/pattern.png'),
+                repeat: ImageRepeat.repeat,
               ),
-              SizedBox(height: 16),
-              SizedBox(
-                width: 200,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            FavoritePage(currentLanguage: currentLanguage),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                MyQRCodePage(currentLanguage: currentLanguage),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurpleAccent,
+                        foregroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        textStyle:
+                            TextStyle(fontSize: 18, fontFamily: 'Helvetica'),
+                        elevation: 4,
+                        shadowColor: Colors.blueAccent.withOpacity(0.3),
                       ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurpleAccent,
-                    foregroundColor: Colors.white,
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
+                      child: Text(_getTranslatedString('QR kodu tara')),
                     ),
-                    textStyle: TextStyle(fontSize: 18, fontFamily: 'Helvetica'),
-                    elevation: 4,
-                    shadowColor: Colors.blueAccent.withOpacity(0.3),
                   ),
-                  child: Text(_getTranslatedString('Favoriler')),
-                ),
+                  SizedBox(height: 16),
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                URLListPage(currentLanguage: currentLanguage),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurpleAccent,
+                        foregroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        textStyle:
+                            TextStyle(fontSize: 18, fontFamily: 'Helvetica'),
+                        elevation: 4,
+                        shadowColor: Colors.blueAccent.withOpacity(0.3),
+                      ),
+                      child: Text(_getTranslatedString('Geçmiş')),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                FavoritePage(currentLanguage: currentLanguage),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurpleAccent,
+                        foregroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        textStyle:
+                            TextStyle(fontSize: 18, fontFamily: 'Helvetica'),
+                        elevation: 4,
+                        shadowColor: Colors.blueAccent.withOpacity(0.3),
+                      ),
+                      child: Text(_getTranslatedString('Favoriler')),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+          if (_isBannerAdReady)
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                width: _bannerAd.size.width.toDouble(),
+                height: _bannerAd.size.height.toDouble(),
+                child: AdWidget(ad: _bannerAd),
+              ),
+            ),
+        ],
       ),
     );
   }
